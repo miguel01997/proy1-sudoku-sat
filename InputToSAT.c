bdec86199778fc4cc9d3dd0701e2570ec83a153f
atoxtough <atoxtough@gmail.com>
2011-02-04 16:41:59 +0000

    InputToSAT.c has been modified. We now use a character matrix.

--- InputToSAT.c
+++ InputToSAT.c
@@ -1,52 +1,40 @@
-#include "bitvector.h"
+#include <stdio.h>
+#include <stdlib.h>
 int order;
 int N;
 int dim;
+char* a;
 
-void fillVector()
-{	int i;
-	int v;
+int matrixFill()
+{	int i,j;
+	char v;
+	i = 0;
+	j = 0;
 	scanf("%d",&order);
 	dim = order*order;
 	N = dim*dim*dim;
-	FOR(i,N)
-		clr(i);
-	i = 0;
-	while( scanf("%d",&v) != EOF)
-	{	if (v == 0){
-			i += dim;
-		}
-		else
-		{
-			i += v-1;
-			set(i);
-			i += dim - v + 1;  
-		}
+	a = malloc(dim*dim*sizeof(char));
+	while( scanf("%c",&v) != EOF)
+	{   if(v == '\n') break;
+	    if(v==' ') continue;
+	    a[i*dim + j] = v;
+	    j += 1;
+	    if(j >= dim){
+		i += 1;
+		j = 0;
+	    }
 	}
+	printf("%s\n",a);
+	if(!(i == dim-1 && j == dim-1))
+	{
+	    free(a);
+	    return -1;
+	}
+	return 0;
 }
 
 
 int main(void){
-	fillVector();
-	int i,j,k,d;
-	i = 0;
-	while(i != 18)
-	{   j = i;
-	    FOR(j,i+dim)
-		if(test(j))
-		{
-		    break;
-		}
-	    printf("j: %d\n",j);
-	    if(j == i+dim)
-	    {	d = 1;
-		FOR(k,i+dim)
-		{   printf("%d ",(i/dim)*100 + k*10 + d);
-		    d++;
-		}
-		printf("%d\n",0);
-	    } 
-	    i += dim;
-	}
+	matrixFill();	
 	return 0;
 }
