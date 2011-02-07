ec53384abee9b5495231bef9cf1e0d004ae84a7a
atoxtough <atoxtough@gmail.com>
2011-02-07 00:08:21 +0000

    Input to SAT translation ready.

--- InputToSAT.c
+++ InputToSAT.c
@@ -4,6 +4,7 @@
 int order;
 int N;
 int dim;
+int V,C,UC;
 char* a;
 
 int matrixFill()
@@ -11,6 +12,7 @@ int matrixFill()
 	char v;
 	i = 0;
 	j = 0;
+	UC = 0;
 	scanf("%d",&order);
 	dim = order*order;
 	N = dim*dim*dim;
@@ -64,62 +66,115 @@ int checkSudoku()
     return 0;
 }
 
+void problem_line()
+{   int sum = dim*(dim-1) - (dim*(dim+1)/2 - 1) + dim - 1;
+    //number of one entry and unit clauses
+    int A = dim*dim;
+    //number of unique entry clauses
+    int B = dim*dim*sum;
+    //number of row,column and subgrid clauses
+    int D = 3*dim*dim*sum;
+    V = dim*dim*dim;
+    C = A + B + D;
+    printf("p cnf %d %d\n",V,C);
+}
+
+void one_number_clause()
+{   int i,j,k;
+    FOR(i,dim)
+    {   FOR(j,dim)
+	{   if(a[i*dim+j] == '0')
+	    {   FOR(k,dim)
+		    printf("%d%d%d ",i+1,j+1,k+1);   
+		printf("%d\n",0);
+	    }
+	    else
+		printf("%d%d%d 0\n",i+1,j+1,a[i*dim+j]-'0');
+	    
+	}
+    }
+}
+/* Given an N^2 x N^2 sudoku matrix produces the clauses
+ * which guarantee that there is no more than one number
+ * per cell */
+
+void unique_number_clause()
+{   int i,j,k,m;
+
+    for(i=1;i<=dim;i++)
+    { // for every cell
+	for(j=1;j<=dim;j++)
+	{/* where the variable count for the current
+	* cell starts */
+	/* create a clause for every possible pair of
+	* variables that refer to the cell's value */
+	for(k=1; k<dim; k++)
+	    for(m=k+1; m<=dim; m++)
+		printf("-%d%d%d -%d%d%d 0\n",i,j,k,i,j,m);
+	}
+    }
+}
+
+void row_clause()
+{   int y,z,x,i;
+    for(y=1;y<dim+1;y++)
+    	for(z=1;z<dim+1;z++)
+	    for(x=1;x<dim;x++)
+    	    	for(i=x+1;i<dim+1;i++)
+		    printf("-%d%d%d -%d%d%d 0\n",x,y,z,i,y,z);	    
+}
+
+void column_clause()
+{   int y,z,x,i;
+    for(x=1;x<dim+1;x++)
+    	for(z=1;z<dim+1;z++)
+	    for(y=1;y<dim;y++)
+    	    	for(i=y+1;i<dim+1;i++)
+		    printf("-%d%d%d -%d%d%d 0\n",x,y,z,x,i,z);	    
+}
+
+void subgrid_clause()
+{   int z,i,j,x;
+    int y,k,l;
+    for(z=1;z<dim+1;z++)
+	for(i=0;i<order;i++)
+	    for(j=0;j<order;j++)
+		for(x=1;x<=order;x++)
+		    for(y=1;y<=order;y++)
+			for(k=y+1;k<=order;k++)
+			{   printf("-%d%d%d -%d%d%d 0\n",
+				    order*i+x,order*j+y,z,
+				    order*i+x,order*j+k,z);
+			}
+    
+    for(z=1;z<dim+1;z++)
+	for(i=0;i<order;i++)
+	    for(j=0;j<order;j++)
+		for(x=1;x<=order;x++)
+		    for(y=1;y<=order;y++)
+			for(k=y+1;k<=order;k++)
+			    for(l=1;l<=order;l++)
+			    {	printf("-%d%d%d -%d%d%d 0\n",
+					order*i+x,order*j+y,z,
+					order*i+k,order*j+l,z);
+			    }	
+}
+
+
 int main(void)
-{	int i,j,k;
-	if(matrixFill() == -1)
+{	if(matrixFill() == -1)
 	{   printf("Bad input\n");    
 	    return 0;
 	}
 	if(checkSudoku() == -1)
 	{   printf("Invalid Sudoku configuration\n");
 	    return 0;
-	}	
-	FOR(i,dim)
-	{   FOR(j,dim)
-	    {	if(a[i*dim+j] == '0')
-		{   FOR(k,dim)
-			printf("%d%d%d ",i+1,j+1,k+1);   
-		    printf("%d\n",0);
-		}
-		else
-		    printf("%d%d%d 0\n",i+1,j+1,a[i*dim+j]-'0');
-	    }
 	}
+	problem_line();	
+	one_number_clause();
+	unique_number_clause();
+	row_clause();
+	column_clause();
+	subgrid_clause();
 	return 0;
-}
-
-
-
-/* Given an N^2 x N^2 sudoku matrix produces the clauses
- * which guarantee that there is no more than one number
- * per cell */
-
-int unique_clause(char* sudoku, int N){
-  int dim = N*N;
-  int cell;
-  int var_start;
-
-  for(int i=0; i<dim; i++){ // for every cell
-    for(int j=0; j<dim; j++){
-
-      cell = i*dim + j; //current cell
-
-      /* where the variable count for the current
-       * cell starts */
-      var_start = cell*dim;
-
-      /* create a clause for every possible pair of
-       * variables that refer to the cell's value */
-      for(int k=1; k<dim; k++){ 
-        for(int m=k+1; m<=dim; m++){
-          printf("%d %d 0", -(var_start+k), -(var_start+m));
-        }
-
-
-      }
-
-
-    }
-  }
-
 }
