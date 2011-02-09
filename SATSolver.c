7deacae2673dd7f26fd96af12db57b2c5c6f2d02
atoxtough <atoxtough@gmail.com>
2011-02-09 21:44:14 +0000

    SATSolver version 1. Compiles but segs fault.

--- SATSolver.c
+++ SATSolver.c
@@ -1,42 +1,114 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_LINE_LENGTH 65536
 
 typedef struct{
+	int* array;
+	int size;
+} rarray_variable;
+
+typedef struct{
     int w_1_i;
     int w_2_i;
-    int* literals;
+    rarray_variable literals;
 } clause;
 
 typedef struct{
-    clause** pW;
-    clause** nW;
+    clause** array;
+    int size;
+} rarray_clause;
+
+typedef struct{
+    rarray_clause pW;
+    rarray_clause nW;
 } variable;
 
- int main(void){
-   FILE* fp;
-   char header[50];
-   char line_buffer[MAX_LINE_LENGTH];
-   char word_buffer[MAX_WORD_LENGTH];
-   char* var, wp;
-   int *clause_buffer;
-   int V,C;
-   int i, num_var;
-   int so_far = 0;
-    variable* VarList;
-    
-    fp = fopen("result.out","r");
-    
-    fscanf(fp,"%*s%*s%d%d",&V,&C);
-    
-    if((VarList = (variable*)malloc(sizeof(variable)*V))==0){
-      printf("Out of memory. Too many variables.\n");
-      exit(1);
+
+int updateVariableArray(rarray_clause* W,clause* n)
+{
+    W->size += 1;
+    clause **_tmp = (clause**) realloc(W->array,
+		(W->size)*sizeof(clause*));
+    if(!_tmp)
+    {
+	printf("Ran out of memory.\n");
+	return -1;
     }
-      
+    W->array = _tmp;
+    W->array[W->size-1] = n;
+    return 0;
+}
+
+ int main(void){
+    /*FILE* fp;
+    char header[50];
+    char line_buffer[MAX_LINE_LENGTH];
+    char word_buffer[MAX_WORD_LENGTH];
+    char* var, wp;
+    int *clause_buffer;
+    int i, num_var;
+    int so_far = 0;*/
+
+    int V,C;
+    scanf("p cnf %d%d",&V,&C);
+    //To count from 1 to N.
+    clause clause_array[C+1];
+    variable variable_array[V+1];
+    int v;
+    int I;
+    clause* n;
+    I = 0;
+    n->literals.array = NULL;
+    n->literals.size = 0;
+    //Variable array is initialized by default.
+    //Clause construction.
+    while(scanf("%d",&v) != EOF)
+    {	if(v == 0) 
+	{   //Actualize variable array.
+	    int w1 = n->literals.array[0];
+	    int w2 = n->literals.array[n->literals.size-1];
+	    if(w1 > 0)
+		if(updateVariableArray(&variable_array[w1].pW,n) == -1)
+		    return -1;
+	    else
+		if(updateVariableArray(&variable_array[-w1].nW,n) == -1)
+		    return -1;
+	    if(w2 > 0)
+		if(updateVariableArray(&variable_array[w2].pW,n) == -1)
+		    return -1;
+	    else
+		if(updateVariableArray(&variable_array[-w2].nW,n) == -1)
+		    return -1;
+	    //insert clause into array. Start new clause.
+	    clause_array[I] = *n;
+	    I++;
+	    clause* p;
+	    p->w_1_i = 0;
+	    p->w_2_i = 0;
+	    p->literals.array = NULL;
+	    p->literals.size = 0;
+	    n = p;
+	}  
+	// v is a value of a variable in a clause.
+	// I must add it to the current clause.			
+	else
+	{   n->literals.size += 1; 
+	    int *_tmp = (int*) realloc(n->literals.array,
+			n->literals.size*sizeof(int));
+	    if(!_tmp)
+	    {
+		printf("Ran out of memory.\n");
+		return -1;
+	    }
+	    n->literals.array = _tmp;
+	    n->literals.array[n->literals.size-1] = v;
+	}
+    } 
+	
     //create the first clause structure
     
-    so_far = 1;          
+    /*so_far = 1;          
     
     while(fgets(buffer, MAX_LINE_LENGTH, fp)){
 
@@ -56,6 +128,7 @@ typedef struct{
         //this marks the end of a clause
         
         //create a new empty clause
+	
       }
 
       wp = word_buffer;
@@ -82,7 +155,8 @@ typedef struct{
     }
 
     //the EOF has been reached, all clauses and variables are ready
-
+    */
+    return 0;
  }
 
 
