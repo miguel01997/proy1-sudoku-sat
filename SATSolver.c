853901af27945f42edc208dd4df5e9e66ecd9fd1
atoxtough <atoxtough@gmail.com>
2011-02-11 15:20:07 +0000

    SATSolver with new approach

--- SATSolver.c
+++ SATSolver.c
@@ -28,14 +28,13 @@ typedef struct{
 int updateVariableArray(rarray_clause* W,clause* n)
 {
     W->size += 1;
-    clause **_tmp = (clause**) realloc(W->array,
+    W->array = (clause**) realloc(W->array,
 		(W->size)*sizeof(clause*));
-    if(!_tmp)
+    if(W->array == NULL)
     {
 	printf("Ran out of memory.\n");
 	return -1;
     }
-    W->array = _tmp;
     W->array[W->size-1] = n;
     return 0;
 }
@@ -55,30 +54,50 @@ int updateVariableArray(rarray_clause* W,clause* n)
     //To count from 1 to N.
     clause clause_array[C+1];
     variable variable_array[V+1];
+    int buffer_variables[V];
     int v;
     int i;
     int I;
-    clause* n;
+    int varcount;
+    /*clause* n;
     if((n = malloc(sizeof(clause))) == NULL)
     {	printf("Out of heap memory.\n");
 	return -1;
     }
     I = 1;
-    n->literals.array = NULL;
-    n->literals.size = 0;
+    n->literals.array = malloc(sizeof(int));
+    if(n->literals.array == NULL)
+    {	printf("Out of memory\n");
+	return -1;
+    }
+    n->literals.size = 0;*/
     //Variable array is initialized by default.
     //Clause construction.
+    I = 1;
+    varcount = 0;
      
     while(scanf("%d",&v) != EOF)
     {	if(v == 0) 
 	{   //Actualize variable array.
 	    
-	    int w1 = n->literals.array[0];
+	    /*int w1 = n->literals.array[0];
 	    int w2 = n->literals.array[n->literals.size-1];
 
 	    n->w_1_i = w1;
-	    n->w_2_i = w2;	    	
+	    n->w_2_i = w2;	    	*/
 
+	    clause n;
+	    int k;
+	    if((n.literals.array = malloc(varsize*sizeof(int))) == NULL)
+	    {	printf("Out of memory");
+		return -1;
+	    }
+	    
+	    n.
+	    for(k = 0;k<varsize;k++)
+		n.literals.array[k] = buffer_variables[k];
+
+	    
 	    if(w1 > 0)
 	    {	if(updateVariableArray(&variable_array[w1].pW,n) == -1)
 		    return -1;
@@ -97,7 +116,7 @@ int updateVariableArray(rarray_clause* W,clause* n)
 	    }
 	    //insert clause into array. Start new clause.
 	    
-	    clause_array[I] = *n;
+	    /*clause_array[I] = *n;
 	    I++;
 	    clause* p;
 	    if((p = malloc(sizeof(clause))) == NULL)
@@ -106,40 +125,45 @@ int updateVariableArray(rarray_clause* W,clause* n)
 	    }	
 	    p->w_1_i = 0;
 	    p->w_2_i = 0;
-	    p->literals.array = NULL;
+	    p->literals.array = malloc(sizeof(int));
+	    if(p->literals.array == NULL)
+	    {	printf("Out of heap memory\n");
+		return -1;
+	    }
 	    p->literals.size = 0;
-	    n = p;
+	    n = p;*/
 	}  
 	// v is a value of a variable in a clause.
 	// I must add it to the current clause.			
 	else
-	{   n->literals.size += 1; 
-	    int *_tmp = (int*) realloc(n->literals.array,
+	{   /*n->literals.size += 1; 
+	    n->literals.array = (int*) realloc(n->literals.array,
 			n->literals.size*sizeof(int));
-	    if(!_tmp)
+	    if(n->literals.array == NULL)
 	    {
 		printf("Ran out of memory.\n");
 		return -1;
 	    }
-	    n->literals.array = _tmp;
-	    n->literals.array[n->literals.size-1] = v;
-
+	    n->literals.array[n->literals.size-1] = v;*/
+	    buffer_variables[varcount] = v;
+	    varcount++;	    
 	}
+	//printf("%d %d %d\n",I,clause_array[2].literals.size,clause_array[2640].literals.size);
     } 
-
-int j;
-for(j=1;j<=C;j++)
-{   printf("Clause %d:\n",j);
-    printf("\tw_1_i: %d w_2_i: %d\n",clause_array[j].w_1_i,clause_array[j].w_2_i);
-    int k;
-    printf("Literals: ");
-    for(k=0;k<clause_array[j].literals.size;k++)
-	printf("%d ",clause_array[j].literals.array[k]);	
-    printf("Number of literals %d\n",clause_array[j].literals.size);
-    printf("\n");
-}	
-
-
+    
+    /*
+    int j;
+    for(j=1;j<=C;j++)
+    {   printf("Clause %d:\n",j);
+	printf("\tw_1_i: %d w_2_i: %d\n",clause_array[j].w_1_i,clause_array[j].w_2_i);
+	int k;
+	printf("Literals: ");
+	for(k=0;k<clause_array[j].literals.size;k++)
+	    printf("%d ",clause_array[j].literals.array[k]);	
+	printf("Number of literals %d\n",clause_array[j].literals.size);
+	printf("\n");
+    }	
+    */
     //create the first clause structure
     
     /*so_far = 1;          
