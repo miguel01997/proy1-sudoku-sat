e9e05fc904872a1421ec849b0abf402d4bef851e
danielbruzual <danielbruzual@d9c1ab8f-4bc3-e104-167c-4d83b838a28d>
2011-02-11 19:44:47 +0000

    Clauses work :-). No more segmentation fault, invalid free, out of memory, no more junk.

--- trunk/SATSolver.c
+++ trunk/SATSolver.c
@@ -28,6 +28,8 @@ typedef struct{
 int updateVariableArray(rarray_clause* W,clause* n)
 {
     W->size += 1;
+
+ 
     W->array = (clause**) realloc(W->array,
 		(W->size)*sizeof(clause*));
     if(W->array == NULL)
@@ -40,195 +42,104 @@ int updateVariableArray(rarray_clause* W,clause* n)
 }
 
  int main(void){
-    /*FILE* fp;
-    char header[50];
-    char line_buffer[MAX_LINE_LENGTH];
-    char word_buffer[MAX_WORD_LENGTH];
-    char* var, wp;
-    int *clause_buffer;
-    int i, num_var;
-    int so_far = 0;*/
+
+
 
     int V,C;
     scanf("p cnf %d%d",&V,&C);
     //To count from 1 to N.
-    clause clause_array[C+1];
+    clause * clause_array[C+1];
     variable variable_array[V+1];
     int buffer_variables[V];
     int v;
     int i;
     int I;
     int varcount;
-    /*clause* n;
-    if((n = malloc(sizeof(clause))) == NULL)
-    {	printf("Out of heap memory.\n");
-	return -1;
+
+    int k;
+    for(k = 1; k<= V; k++){
+      variable_array[k].pW.array = 0;
+      variable_array[k].nW.array = 0;
+      variable_array[k].pW.size = 0;
+      variable_array[k].nW.size = 0;
     }
-    I = 1;
-    n->literals.array = malloc(sizeof(int));
-    if(n->literals.array == NULL)
-    {	printf("Out of memory\n");
-	return -1;
-    }
-    n->literals.size = 0;*/
-    //Variable array is initialized by default.
-    //Clause construction.
+
     I = 1;
     varcount = 0;
      
+    clause * n1;
     while(scanf("%d",&v) != EOF)
     {	if(v == 0) 
-	{   //Actualize variable array.
-	    
-	    /*int w1 = n->literals.array[0];
-	    int w2 = n->literals.array[n->literals.size-1];
-
-	    n->w_1_i = w1;
-	    n->w_2_i = w2;	    	*/
-
-	    clause n;
+	{   
 	    int k;
 	    int w1,w2;
+	    
+	    n1 = malloc(sizeof(clause));
 
-	    if((n.literals.array = malloc(varcount*sizeof(int))) 
+	    
+
+	    if((n1->literals.array = malloc(varcount*sizeof(int))) 
 				   == NULL)
 	    {	printf("Out of memory");
 		return -1;
 	    }
+	   
 	    
-	    n.literals.size = varcount;
+	    n1->literals.size = varcount;
 	    for(k = 0;k<varcount;k++)
-		n.literals.array[k] = buffer_variables[k];
-	    n.w_1_i = 0;
-	    n.w_2_i = varcount - 1;
+		n1->literals.array[k] = buffer_variables[k];
+	    n1->w_1_i = 0;
+	    n1->w_2_i = varcount - 1;
 
-	    w1 = n.literals.array[n.w_1_i];
-            w2 = n.literals.array[n.w_2_i];
+	    w1 = n1->literals.array[n1->w_1_i];
+            w2 = n1->literals.array[n1->w_2_i];
 
-	    if( w1 > 0)
-	    {	if(updateVariableArray(&variable_array[w1].pW,
-				       &n) == -1)
+	    	    if( w1 > 0)
+	      {	if(updateVariableArray(&(variable_array[w1].pW),
+				       n1) == -1)
 		    return -1;
 	    }
 	    else
-	    {	if(updateVariableArray(&variable_array[-w1].nW,
-				       &n) == -1)
+	      {	if(updateVariableArray(&(variable_array[-w1].nW),
+				       n1) == -1)
 		    return -1;
 	    }
 	    if(w2 > 0)
-	    {	if(updateVariableArray(&variable_array[w2].pW,
-				       &n) == -1)
+	      {	if(updateVariableArray(&(variable_array[w2].pW),
+				       n1) == -1)
 		    return -1;
 	    }
 	    else
-	    {	if(updateVariableArray(&variable_array[-w2].nW,
-				       &n) == -1)
+	      {	if(updateVariableArray(&(variable_array[-w2].nW),
+				       n1) == -1)
 		    return -1;
-	    }
+		    }
 
-	    clause_array[I] = n;    
+	    clause_array[I] = n1;
+
 	    varcount = 0;
 	    I++;
-	    //insert clause into array. Start new clause.
 	    
-	    /*clause_array[I] = *n;
-	    I++;
-	    clause* p;
-	    if((p = malloc(sizeof(clause))) == NULL)
-	    {	printf("Out of heap memory\n");
-		return -1;
-	    }	
-	    p->w_1_i = 0;
-	    p->w_2_i = 0;
-	    p->literals.array = malloc(sizeof(int));
-	    if(p->literals.array == NULL)
-	    {	printf("Out of heap memory\n");
-		return -1;
-	    }
-	    p->literals.size = 0;
-	    n = p;*/
-	}  
-	// v is a value of a variable in a clause.
-	// I must add it to the current clause.			
-	else
-	{   /*n->literals.size += 1; 
-	    n->literals.array = (int*) realloc(n->literals.array,
-			n->literals.size*sizeof(int));
-	    if(n->literals.array == NULL)
-	    {
-		printf("Ran out of memory.\n");
-		return -1;
-	    }
-	    n->literals.array[n->literals.size-1] = v;*/
-	    buffer_variables[varcount] = v;
-	    varcount++;	    
-	}
-	//printf("%d %d %d\n",I,clause_array[2].literals.size,clause_array[2640].literals.size);
+	}else {
+	
+	buffer_variables[varcount] = v;
+	varcount++;	    
+      }
+	
     } 
     
     
     int j;
     for(j=1;j<=C;j++)
     {   printf("Clause %d:\n",j);
-	printf("\tw_1_i: %d w_2_i: %d\n",clause_array[j].w_1_i,clause_array[j].w_2_i);
+	printf("\tw_1_i: %d w_2_i: %d\n",clause_array[j]->w_1_i,clause_array[j]->w_2_i);
 	int k;
 	printf("Literals: ");
-	for(k=0;k<clause_array[j].literals.size;k++)
-	    printf("%d ",clause_array[j].literals.array[k]);	
-	printf("Number of literals %d\n",clause_array[j].literals.size);
+	for(k=0;k<clause_array[j]->literals.size;k++)
+	    printf("%d ",clause_array[j]->literals.array[k]);	
+	printf("Number of literals %d\n",clause_array[j]->literals.size);
 	printf("\n");
-    }	
-    
-    //create the first clause structure
-    
-    /*so_far = 1;          
-    
-    while(fgets(buffer, MAX_LINE_LENGTH, fp)){
-
-      
-      if(buffer[0]=='c'){ //ignore comments in input file
-        continue;
-      }
-      
-      var = buffer;
-      
-      //skip through spaces and such
-      while(*var != 0 && (*var == '\t' || *var == '\r' || *var == ' ')){
-        var++;
-      }  
-      
-      if(*var == '0'){
-        //this marks the end of a clause
-        
-        //create a new empty clause
-	
-      }
-
-      wp = word_buffer;
-
-      //copy all the digits (and negation sign) to the word buffer
-      while(*var != 0 && *var != '\t' && *var != '\r' && *var != ' '){
-        *wp = *var;
-        wp++;
-        var++;
-      }
-      wp++;
-      *wp = '\0';
-
-      sscanf(word_buffer, "%d", &num_var); //convert the number to binary
-
-      //add literal to clause
-
-      //create variable and add pointer to the clause.
-      //if int value is < 0 then it is negated
-      //otherwise it is positive
-
-      
-      
     }
-
-    //the EOF has been reached, all clauses and variables are ready
-    */
     return 0;
  }
 
